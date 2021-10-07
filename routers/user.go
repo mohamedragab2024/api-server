6c1a809de9b51eca115d779053d0880286f030c3
Mohamed Ragab <mohamed.ragab.ext@aptar.com>
2021-10-07 22:34:22 +0200

    add delete in users api

--- routers/user.go
+++ routers/user.go
@@ -14,6 +14,8 @@ func (r UserRouter) Handle(router *mux.Router) {
 	router.HandleFunc("/users/{id}", controller.GetOne).Methods(httpverbs.MethodGet)
 	router.HandleFunc("/users/{id}", controller.Delete).Methods(httpverbs.MethodDelete)
 	router.HandleFunc("/users", controller.Create).Methods(httpverbs.MethodPost)
+	router.HandleFunc("/users", controller.Update).Methods(httpverbs.MethodPut)
+
 	router.HandleFunc("/users/changePassword/", controller.ChangePassword).Methods(httpverbs.MethodPut)
 	//handle as well / at the end
 	router.HandleFunc("/users/", controller.GetAll).Methods(httpverbs.MethodGet)
